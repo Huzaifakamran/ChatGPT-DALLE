b3d232ef6d35b2e76a105328efd412d343caeb31
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-04-10 07:11:23 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import openai
+import os
+import urllib.request
+from PIL import Image
+import streamlit as st
+# from dotenv import load_dotenv
+# load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_image(image_description):
+
+  img_response = openai.Image.create(
+    prompt = image_description,
+    n=1,
+    size="512x512")
+  
+
+  img_url = img_response['data'][0]['url']
+
+  urllib.request.urlretrieve(img_url, 'img.png')
+
+  img = Image.open("img.png")
+  
+  return img
+
+
+
+# page title
+st.title('DALL.E - Image Generation - OpenAI')
+
+#Insert API KEY
+openai.api_key = st.text_input('API-KEY',type='password')
+
+# text input box for image recognition
+img_description = st.text_input('Image Desription')
+
+
+if st.button('Generate Image'):
+    
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
+    
